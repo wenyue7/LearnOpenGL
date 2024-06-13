751e0791e6c72f485cd0ea48eb989278bea39e3a
Hongjin Li <vic.hong@rock-chips.com>
2024-06-13 10:26:17 +0800

    add opengl opencv display demo

---
+++ Demo/12.openglOpenCVDisplay/compile.sh
@@ -0,0 +1,1 @@
+g++ Textures.cpp -o a.out $(pkg-config --libs --cflags gl glew glfw3 glib-2.0 glproto glu opencv) -lGLEW  -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -lglib-2.0 -lGLU -ldl -lglut -lSOIL -lXxf86vm -lXinerama -lXcursor && ./a.out
